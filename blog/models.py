af114dce42ff1d14f530c94ab75090dbe081552f
Andrew Kramer <kramer08@gmail.com>
2016-12-04 20:13:21 -0600

    fixed error in datetime.now() in Post Model. changed comment sort to most recent first.

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(
-        default=timezone.now)
+        default=timezone.now())
     published_date = models.DateTimeField(
         blank=True, null=True)
     
@@ -31,6 +31,9 @@ class Comment(models.Model):
     created_date = models.DateTimeField(default=timezone.now())
     approved_comment = models.BooleanField(default=False)
     
+    class Meta:
+        ordering = ['-created_date']
+        
     def approve(self):
         self.approved_comment = True
         self.save()
